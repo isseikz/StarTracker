48f83d4dedb4ed5e86faaea7e0b35fe686e4764a
isseikz <kuzumaki-issei-sk@ynu.jp>
2018-05-27 11:51:32 +0900

    display tested images

--- binmom.py
+++ binmom.py
@@ -15,7 +15,7 @@ def binalize(img):
 
 def getMoments(img):
     mu = cv2.moments(img, False)
-    x,y= int(mu["m10"]/mu["m00"]) , int(mu["m01"]/mu["m00"])
+    x,y= int(mu["m10"]/mu["m00"]) ,int(mu["m01"]/mu["m00"])
 
     return np.array([x,y])
 
@@ -27,6 +27,7 @@ def run(img):
     center = getMoments(img_binalized)
 
     error = center - img_size/2
+    error[1]=-error[1]
 
     print("Position: [%d, %d]" % (center[0],center[1]))
     print("Error: [%d, %d]" % (error[0], error[1]))
@@ -34,9 +35,26 @@ def run(img):
     return error, center
 
 if __name__ == '__main__':
+    img = cv2.imread('./test/IMAGE_MOON.JPG',1)
+    error, center = run(img)
+
+    cv2.circle(img, (center[0],center[1]), int(img.shape[1] * 0.01), (0, 0, 255), -1)
+    plt.figure()
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+    plt.show()
+
+    img = cv2.imread('./test/IMAGE_MOON2.JPG',1)
+    error, center = run(img)
+
+    cv2.circle(img, (center[0],center[1]), int(img.shape[1] * 0.01), (0, 0, 255), -1)
+    plt.figure()
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
+    plt.show()
+
     img = cv2.imread('./test/IMAGE_MOON3.JPG',1)
     error, center = run(img)
 
-    cv2.circle(img, (center[0],center[1]), 4, 100, 2, 4)
+    cv2.circle(img, (center[0],center[1]), int(img.shape[1] * 0.01), (0, 0, 255), -1)
+    plt.figure()
     plt.imshow(cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
     plt.show()
