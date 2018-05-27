8496ae9693895d81c71f35aeda6bac80b1f90bf6
isseikz <kuzumaki-issei-sk@ynu.jp>
2018-05-27 11:59:04 +0900

    added serial control

--- controlViaSerial.py
+++ controlViaSerial.py
@@ -2,26 +2,27 @@ import serial
 
 class SerialCTRl(object):
     """docstring for SerialCTRl."""
-    def __init__(self, port, baudrate=115200, to=10):
-        self.s = serial.Serial(port,baudrate,timeout=to)
-        self.isSending = false
+    # def __init__(self, p, br=115200, to=10):
+    def __init__(self, serial):
+        self.s = serial
+        self.isSending = False
 
-    def write(data):
+    def write(self,data):
         self.s.write(data)
-        s.close
+        self.s.close
 
-    def read():
+    def read(self):
         res = self.s.readline()
         return res
 
-    def controlMCU(data):
-        if isSending == False:
-            isSending = True
-            write(data)
-            s.close()
+    def controlMCU(self,data):
+        if self.isSending == False:
+            self.isSending = True
+            self.s.write(data)
+            self.s.close()
 
             while True:
-                res = read()
+                res = self.read()
                 if res[4] == 0:
                     self.isSending = false
                     if res[6] == 0:
@@ -30,4 +31,15 @@ class SerialCTRl(object):
             return False
 
 if __name__ == '__main__':
-    main()
+    startCode = [17,34,51,68]
+    flag = 16
+    dataSize = 2
+
+    ser = serial.Serial("COM10", 115200)
+    com = SerialCTRl(ser)
+
+    sentData = bytearray(b'\x11\x22\x33\x44\x10\x02\x01\x0a\r\n')
+    res = com.write(sentData)
+
+    sentData = bytearray(b'\x11\x22\x33\x44\x10\x02\x00\x05\r\n')
+    res = com.write(sentData)
