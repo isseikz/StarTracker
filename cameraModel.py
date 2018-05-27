99917a21021b9e5e14fb40a44b1e26f6ea5ec421
isseikz <kuzumaki-issei-sk@ynu.jp>
2018-05-27 15:13:30 +0900

    debug display component

--- cameraModel.py
+++ cameraModel.py
@@ -11,18 +11,20 @@ class CameraModel(object):
         super(CameraModel, self).__init__()
         self.initialImage   = ii
         self.currentImage   = ii
-        self.position       = [0,0]    #
-        self.planetVelocity = [10,2] # [pixel/step]
-        # self.planetVelocity = [0,0] # [pixel/step]
-        self.imageSize = tuple(np.array([self.initialImage.shape[1], self.initialImage.shape[0]]))
+        # self.planetVelocity = [10,-2] # [pixel/step]
+        self.planetVelocity = [0,0] # [pixel/step]
+        self.imageSize      = tuple(np.array([self.initialImage.shape[1], self.initialImage.shape[0]]))
+        self.position       = [0,0]
+        self.gearRate       = 1.5
 
     def updateImage(self, posdX, posdY):
-        self.position[0] += posdX + self.planetVelocity[0]
-        self.position[1] += posdY + self.planetVelocity[1]
+        print([posdX,posdY])
+        self.position[0] += posdX * self.gearRate + self.planetVelocity[0]
+        self.position[1] += posdY * self.gearRate + self.planetVelocity[1]
         rad = 0
         matrix = [
         [np.cos(rad), -np.sin(rad), self.position[0]],
-        [np.sin(rad), np.cos(rad), -self.position[1]]
+        [np.sin(rad), np.cos(rad),  self.position[1]]
         ]
         affineMatrix = np.float32(matrix)
         self.currentImage = cv2.warpAffine(self.initialImage, affineMatrix, self.imageSize,flags=cv2.INTER_NEAREST)
@@ -30,15 +32,14 @@ class CameraModel(object):
     def showCurrentImage(self):
         cv2.namedWindow('Current Image', cv2.WINDOW_NORMAL)
 
-        outputImage = self.currentImage
+        outputImage = self.currentImage.copy() #OpenCV は別の変数に代入しても，元の変数を改変するため，.copy()関数でクローンを作成する
         cv2.line(outputImage, (self.imageSize[0]//2,self.imageSize[1]//2-20), (self.imageSize[0]//2,self.imageSize[1]//2+20), (200, 200, 200),thickness=3, lineType=cv2.LINE_AA)
         cv2.line(outputImage, (self.imageSize[0]//2-20,self.imageSize[1]//2), (self.imageSize[0]//2+20,self.imageSize[1]//2), (200, 200, 200),thickness=3,lineType=cv2.LINE_AA)
-        cv2.putText(outputImage, 'X POSITION: ' + str(self.position[0]), (20, 70), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 3, cv2.LINE_AA)
-        cv2.putText(outputImage, 'Y POSITION: ' + str(self.position[1]), (20, 120), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 3, cv2.LINE_AA)
-        cv2.putText(outputImage, 'STOP: \'s\'', (20, 170), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 3, cv2.LINE_AA)
-        cv2.putText(outputImage, 'CONTROL: \'jil,\'', (20, 220), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 3, cv2.LINE_AA)
+        cv2.putText(outputImage, 'X POSITION: ' + str(self.position[0]), (40, 70), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 3, cv2.LINE_AA)
+        cv2.putText(outputImage, 'Y POSITION: ' + str(self.position[1]), (40, 120), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 3, cv2.LINE_AA)
+        cv2.putText(outputImage, 'STOP: \'s\'', (40, 170), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 3, cv2.LINE_AA)
+        cv2.putText(outputImage, 'CONTROL: \'jil,\'', (40, 220), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 3, cv2.LINE_AA)
         cv2.imshow('Current Image',outputImage)
-
 
 
 if __name__ == '__main__':
@@ -48,9 +49,6 @@ if __name__ == '__main__':
     c = 'o'
     ctrl = [0,0]
     while True:
-        error, center = binmom.run(model.currentImage)
-        print(ctrl)
-
         key = cv2.waitKey(100)&0xff
         if key == ord('s'):
             break
@@ -65,7 +63,8 @@ if __name__ == '__main__':
         else:
             ctrl = [0,0]
 
-        ctrl -= 1.5* error // 1
+        error, center = binmom.run(model.currentImage)
+        ctrl = -1.2 * error
 
         model.updateImage(*ctrl)
         model.showCurrentImage()
